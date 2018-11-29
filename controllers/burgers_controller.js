81e0331cee673df473a0973a6a2ccd486e2608b0
brintzamiles <brintza@bmorinnovative.com>
2018-11-29 14:55:22 -0500

    more styling/reworking/added reorder feature

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -47,6 +47,25 @@ router.put("/api/devourburger/:id", function(req, res) {
 });
 
 
+router.put("/api/reorderburger/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("burgerId", req.params.id);
+
+  console.log("condition", condition);
+
+  //changes the status of an eaten or devoured burger
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
 
 
 // Export routes for server.js to use.
